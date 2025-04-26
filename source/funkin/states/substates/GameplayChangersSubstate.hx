dcaf8fa56e902fdba56a88320aef8b39c277ab62
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2025-04-26 22:49:46 +0800

    Update GameplayChangersSubstate.hx

--- source/funkin/states/substates/GameplayChangersSubstate.hx
+++ source/funkin/states/substates/GameplayChangersSubstate.hx
@@ -136,6 +136,9 @@ class GameplayChangersSubstate extends MusicBeatSubstate
 
 		changeSelection();
 		reloadCheckboxes();
+
+		addTouchPad("LEFT_FULL", "A_B_C");
+		addTouchPadCamera();
 	}
 
 	var nextAccept:Int = 5;
@@ -283,7 +286,7 @@ class GameplayChangersSubstate extends MusicBeatSubstate
 				}
 			}
 
-			if (controls.RESET)
+			if (controls.RESET || touchPad.buttonC.justPressed)
 			{
 				for (i in 0...optionsArray.length)
 				{
