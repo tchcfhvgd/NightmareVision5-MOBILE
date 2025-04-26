0548261d2b001c9ced1ddc05726c2ccd05df1a86
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2025-04-26 22:13:23 +0800

    Update CopyState.hx

--- source/mobile/CopyState.hx
+++ source/mobile/CopyState.hx
@@ -70,7 +70,7 @@ class CopyState extends MusicBeatState
 		checkExistingFiles();
 		if (maxLoopTimes <= 0)
 		{
-			MusicBeatState.switchState(new TitleState());
+			FlxG.switchState(new Splash());
 			return;
 		}
 
@@ -122,7 +122,7 @@ class CopyState extends MusicBeatState
 				canUpdate = false;
 				FlxG.sound.play(Paths.sound('confirmMenu')).onComplete = () ->
 				{
-					MusicBeatState.switchState(new TitleState());
+					FlxG.switchState(new Splash());
 				};
 			}
 
@@ -218,7 +218,7 @@ class CopyState extends MusicBeatState
 
 		// removes unwanted assets
 		var assets = locatedFiles.filter(folder -> folder.startsWith('assets/'));
-		var mods = locatedFiles.filter(folder -> folder.startsWith('mods/'));
+		var mods = locatedFiles.filter(folder -> folder.startsWith('content/'));
 		locatedFiles = assets.concat(mods);
 		locatedFiles = locatedFiles.filter(file -> !FileSystem.exists(file));
 
