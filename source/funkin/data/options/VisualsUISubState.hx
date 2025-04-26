4838d7b70f0dd66dd9637758a75ccc1782bc002b
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2025-04-26 23:38:48 +0800

    Update VisualsUISubState.hx

--- source/funkin/data/options/VisualsUISubState.hx
+++ source/funkin/data/options/VisualsUISubState.hx
@@ -70,10 +70,8 @@ class VisualsUISubState extends BaseOptionsMenu
 		super.destroy();
 	}
 
-	#if !mobile
 	function onChangeFPSCounter()
 	{
 		if (Main.fpsVar != null) Main.fpsVar.visible = ClientPrefs.showFPS;
 	}
-	#end
 }
