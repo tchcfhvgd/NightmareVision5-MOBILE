54b4f3a6a77f44cfe539472eab8c7db76ff75d72
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2025-04-26 23:01:36 +0800

    Update VisualsUISubState.hx

--- source/funkin/data/options/VisualsUISubState.hx
+++ source/funkin/data/options/VisualsUISubState.hx
@@ -39,11 +39,9 @@ class VisualsUISubState extends BaseOptionsMenu
 		option.decimals = 1;
 		addOption(option);
 
-		#if !mobile
 		var option:Option = new Option('FPS Counter', 'If unchecked, hides FPS Counter.', 'showFPS', 'bool', true);
 		addOption(option);
 		option.onChange = onChangeFPSCounter;
-		#end
 
 		var option:Option = new Option('Pause Screen Song:', "What song do you prefer for the Pause Screen?", 'pauseMusic', 'string', 'Tea Time',
 			['None', 'Breakfast', 'Tea Time']);
