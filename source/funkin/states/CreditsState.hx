83deb18b028969f420d2356fd29839208e155388
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2025-04-26 22:37:49 +0800

    Update CreditsState.hx

--- source/funkin/states/CreditsState.hx
+++ source/funkin/states/CreditsState.hx
@@ -302,6 +302,8 @@ class CreditsState extends MusicBeatState
 		intendedColor = bg.color;
 		changeSelection();
 		super.create();
+
+		addTouchPad("UP_DOWN", "A_B");
 	}
 
 	var quitting:Bool = false;
