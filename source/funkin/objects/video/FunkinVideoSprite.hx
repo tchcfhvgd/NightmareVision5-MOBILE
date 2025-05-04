6e201c122e92568b83c6f73f1de9aa893bd94427
tchcfhvgd <2220687506@qq.com>
2025-05-04 13:04:01 +0800

    fixes

--- source/funkin/objects/video/FunkinVideoSprite.hx
+++ source/funkin/objects/video/FunkinVideoSprite.hx
@@ -1,16 +1,10 @@
 package funkin.objects.video;
 
+#if VIDEOS_ALLOWED
+import hxvlc.flixel.FlxVideoSprite;
 import hxvlc.util.Location;
 
-import flixel.util.FlxSignal;
-
-import hxvlc.flixel.FlxVideoSprite;
-
-// a little funky but thats what happens when u gotta do weird workarounds
-// probably will do more iwth this later
-
-//by data 5 for 17buckys 
-//still a wip version of it tho but its better than the other ones so
+// with hxvlcs improvements this is less needed but still has its values
 
 /**
  * Handles video playback as a `FlxSprite`. Has additional features for ease
@@ -19,152 +13,157 @@ import hxvlc.flixel.FlxVideoSprite;
  * 
  * General Usage:
  * ```haxe
- * 	var video = new FunkinVideoSprite(x,y).preload(videoPath, [FunkinVideoSprite.MUTED])
+ * 	var video = new FunkinVideoSprite(x,y);
  * 	add(video);
- * 	video.onReady.addOnce(()->{
+ * 	video.onFormat(()->{
  * 		video.setGraphicSize(0,FlxG.height);
  * 		video.updateHitbox();
  * 		video.screenCenter(FlxAxes.X);
  * 
  * 	});
- *	video.playVideo();
-
+ * 	if (video.load(Paths.video('pathToVideo')))
+ * 	{
+ *		video.delayAndStart();
+ * 	}
  * ```
  */
-class FunkinVideoSprite extends FlxVideoSprite {
-	public static function init() {
-		hxvlc.util.Handle.init(['--no-lua']);
+class FunkinVideoSprite extends FlxVideoSprite
+{
+	/**
+	 * Video loading argument to make the video loop
+	 * 
+	 * Usage:
+	 * ```haxe
+	 * video.load(Paths.video(''),[FunkinVideoSprite.looping]);
+	 * ```
+	 */
+	public static final looping:String = ':input-repeat=65535';
+	
+	/**
+	 * Video loading argument to make the video muted
+	 * Use if your video doesnt require audio
+	 * 
+	 * Usage:
+	 * ```haxe
+	 * video.load(Paths.video(''),[FunkinVideoSprite.muted]);
+	 * ```
+	 */
+	public static final muted:String = ':no-audio';
+	
+	/**
+	 * Manually initiates the Libvlc instance
+	 */
+	public static function init()
+	{
+		hxvlc.util.Handle.init();
 	}
 	
-	public static var _videos:Array<FunkinVideoSprite> = [];
-	
+
 	/**
-	 * Loading option that makes the video loop
-	 */
-	public static final LOOPING:String = ':input-repeat=65535';
-	
-	/**
-	 * Loading option that mutes the video.
+	 * Bool that decides if `this` should be affected by states
 	 * 
-	 * Use if video audio is not required.
+	 * Disable this if you dont want your video to pause when paused in `PlayState`
 	 */
-	public static final MUTED:String = ':no-audio';
-	
+	public var isStateAffected:Bool = true;
+
 	/**
-	 * Dispatched when the video ends.
-	 * 
-	 * wrapper for `bitmap.onEndReached`
+	 * Creates a new FunkinVideoSprite
+	 * @param x `x` position
+	 * @param y `y` position
+	 * @param oneTimeUse if `true` on video complete, the video will self destroy
 	 */
-	public final onFinish:FlxSignal = new FlxSignal();
-	
-	/**
-	 * Dispatched when the videos internal bitmap is ready.
-	 * 
-	 * 
-	 * wrapper for `bitmap.onFormatSetup`
-	 */
-	public final onReady:FlxSignal = new FlxSignal();
-	
-	/**
-	 * Dispatched when the video starts.
-	 * 
-	 * wrapper for `bitmap.onOpening`
-	 */
-	public final onStart:FlxSignal = new FlxSignal();
-	
-	private var wasPlaying:Bool = false;
-	
-	/**
-	 * @param destroyOnFinish if true, will destroy itself on finish.
-	 */
-	public function new(x:Float = 0, y:Float = 0, destroyOnFinish:Bool = true):Void {
+	public function new(x:Float = 0, y:Float = 0, oneTimeUse:Bool = true)
+	{
 		super(x, y);
 		
-		if (bitmap != null) {
-			bitmap.onFormatSetup.add(onReady.dispatch, false);
-			bitmap.onEndReached.add(onFinish.dispatch, false);
-			
-			bitmap.onOpening.add(() -> {
-				if (!_preloaded)
-					onStart.dispatch(); // being real another option that is less hacky is all together ignore onStart and just do ur stuff when u call play
-			}, false);
-			
-			if (destroyOnFinish)
-				bitmap.onEndReached.add(this.destroy, true, -9999);
-		}
-		_videos.push(this);
-	}
-	
-	public function preload(path:Location, ?options:Array<String>):FunkinVideoSprite {
-		// if u didnt use path try to find the path and the extension
-		if (path is String) {
-			var stringPath:String = cast path;
-			if (!stringPath.endsWith('.mp4') && !stringPath.endsWith('.mov')) {
-				var found = false;
-				for (ext in ['mp4', 'mov']) {
-					
-					final fullPath = 'assets/videos/$stringPath.$ext'; 
-					if (FileSystem.exists(fullPath)) {
-						stringPath = fullPath;
-						found = true;
-						break;
-					}
-				}
-				if (found)
-					path = stringPath;
-			}
-		}
-		
-		// essentially this is a workaround for hxvlc's inconsistent video playback
-		// sometimes its delayed and sometimes loading,playing,stopping to try to preload causes openfl texture corruption garbage
-		if (load(path, options)) {
-			_preloaded = true; // im going to play a risk and assume the video does infact play
-			
-			if (play()) {
-				pause();
-				visible = false;
-				if (bitmap != null)
-					bitmap.time = 0;
-			}
-		}
-		
-		return this;
+		if (oneTimeUse) bitmap.onEndReached.add(this.destroy, true);
 	}
 	
 	/**
-	 * Use over `play()`.
+	 * Starts the video but sets a delay before starting
+	 * 
+	 * Recommended over `this.play`
+	 * @param delay The delay before the video starts. default is next update call
 	 */
-	public function playVideo():Void {
-		FlxTimer.wait(0, _preloaded ? _preloadPlay : play);
+	public function delayAndStart(delay:Float = 0)
+	{
+		FlxTimer.wait(delay, play);
+	}
+
+	/**
+	 * Adds a event to be dispatched when the video reaches its end
+	 * @param func the event to be called
+	 * @param once if this event should be dispatched once, or every time the video ends.
+	 */
+	public function onEnd(func:Void->Void, once:Bool = false)
+	{
+		bitmap.onEndReached.add(func, once);
 	}
 	
-	private var _preloaded:Bool = false;
-	
-	function _preloadPlay():Void {
-		visible = true;
-		resume();
-		_preloaded = false;
-		onStart.dispatch();
+	/**
+	 * Adds a event to be dispatched when the video starts
+	 * @param func the event to be called
+	 * @param once if this event should be dispatched once, or every time the video ends.
+	 */
+	public function onStart(func:Void->Void, once:Bool = false)
+	{
+		bitmap.onOpening.add(func, once);
 	}
 	
-	override function destroy():Void {
-		if (bitmap != null) {
-			bitmap.onEndReached.removeAll();
-			bitmap.onFormatSetup.removeAll();
-			bitmap.onPlaying.removeAll();
+	/**
+	 * Adds a event to be dispatched when the video has formatted itself 
+	 * 
+	 * Recommended to setup ur video during this event
+	 * example: 
+	 * ```haxe
+	 * 	onFormat(()->{
+	 * 		this.scale.set(3,3);
+	 * 		this.updateHitbox();
+	 * 		this.cameras = [camera];
+	 * 	});
+	 * ```
+	 * @param func the event to be called
+	 * @param once if this event should be dispatched once, or every time the video ends.
+	 */
+	public function onFormat(func:Void->Void, once:Bool = false)
+	{
+		bitmap.onFormatSetup.add(func, once);
+	}
+	
+	@:deprecated('use onEnd,onFormat,onStart or access the bitmap for more functions instead.')
+	public function addCallback(vidCallBack:String, func:Void->Void, once:Bool = false)
+	{
+		switch (vidCallBack)
+		{
+			case 'onEnd':
+				bitmap.onEndReached.add(func, once);
+			case 'onStart':
+				bitmap.onOpening.add(func, once);
+			case 'onFormat':
+				bitmap.onFormatSetup.add(func, once);
 		}
-		
-		onReady.removeAll();
-		onReady.destroy();
-		
-		onFinish.removeAll();
-		onFinish.destroy();
-		
-		onStart.removeAll();
-		onStart.destroy();
-		
-		_videos.remove(this);
+	}
+	
+	public static function cacheVid(path:String)
+	{
+		var video = new FunkinVideoSprite(0, 0, false);
+		video.onStart(video.destroy);
+		if (video.load(path, [muted])) video.delayAndStart();
+	}
+	
+	override function destroy()
+	{
+		if (bitmap != null)
+		{
+			bitmap.stop();
+			
+			if (FlxG.signals.focusGained.has(bitmap.resume)) FlxG.signals.focusGained.remove(bitmap.resume);
+			if (FlxG.signals.focusLost.has(bitmap.pause)) FlxG.signals.focusLost.remove(bitmap.pause);
+		}
 		
 		super.destroy();
 	}
 }
+
+#end
+
